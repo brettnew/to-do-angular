aff09c3cba18960e0db3557a25c849917e5a2a18
Brett New and Molly McGlone <brett+molly@>
2016-08-09 08:53:26 -0700

    add pipe and checkboxes to display done and not done tasks.

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,19 +3,21 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
+import { DonePipe } from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  pipes: [DonePipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <select>
+  <select (change)="onChange($event.target.value)" class="filter">
     <option value="all">Show All</option>
     <option value="done">Show Done</option>
     <option value="notDone" selected="selected">Show Not Done</option>
   </select>
-  <task-display *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)" [class.selected]="currentTask === selectedTask" [task]="currentTask">
+  <task-display *ngFor="#currentTask of taskList | done:filterDone" (click)="taskClicked(currentTask)" [class.selected]="currentTask === selectedTask" [task]="currentTask">
   </task-display>
   <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
   </edit-task-details>
@@ -27,6 +29,7 @@ export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
@@ -39,5 +42,9 @@ export class TaskListComponent {
     this.taskList.push(
       new Task(description, this.taskList.length)
     );
+  }
+  onChange(filterOption) {
+    this.filterDone = filterOption;
+    console.log(this.filterDone);
   }
 }
